1b8b1daa92f511bd08b32a0bc15b0c437502af2d
David Oliver <david@doliver.co.uk>
2021-03-16 21:59:48 +0100

    Implement initial play track broadcast

--- lib/live_cue_web/live/cue_live.ex
+++ lib/live_cue_web/live/cue_live.ex
@@ -1,12 +1,21 @@
 defmodule LiveCueWeb.CueLive do
   use LiveCueWeb, :live_view
-  alias LiveCue.Collection
-  alias LiveCueWeb.AlbumComponent
+  alias LiveCue.{Collection, Player}
+  alias LiveCueWeb.{Endpoint, AlbumComponent}
 
   @impl true
   def mount(_params, _session, socket) do
+    if connected?(socket) do
+      Endpoint.subscribe("player")
+    end
+
     index = Collection.get_index()
-    {:ok, assign(socket, :index, index)}
+
+    socket =
+      socket
+      |> assign(:index, index)
+      |> assign(:track_started_playing, nil)
+    {:ok, socket}
   end
 
   @impl true
@@ -18,19 +27,12 @@ defmodule LiveCueWeb.CueLive do
 
   @impl true
   def handle_event("play", %{"album-id" => album_id, "album-type" => album_type, "track-number" => track_number}, socket) do
-    collection_directory = Application.fetch_env!(:live_cue, :collection_directory)
-    track_relative_path =
-      Collection.get_album(album_type, album_id)
-      |> Map.get(:tracks)
-      |> Enum.find(&(&1.number == String.to_integer(track_number)))
-      |> Map.get(:relative_path)
-    track_path = Path.join(collection_directory, track_relative_path)
-
-    System.cmd("cmus-remote", ["--stop"])
-    System.cmd("cmus-remote", ["--clear", "--queue"])
-    System.cmd("cmus-remote", ["--queue", track_path])
-    System.cmd("cmus-remote", ["--next"])
-    System.cmd("cmus-remote", ["--play"])
+    payload = %{
+      album_id: album_id,
+      album_type: album_type,
+      track_number: String.to_integer(track_number)
+    }
+    Endpoint.broadcast("player", "start_playing_track", payload)
 
     {:noreply, socket}
   end
@@ -47,5 +49,12 @@ defmodule LiveCueWeb.CueLive do
     System.cmd("cmus-remote", ["--pause"])
 
     {:noreply, socket}
+  end
+
+  @impl true
+  def handle_info(%{topic: "player", event: "start_playing_track", payload: payload}, socket) do
+    {:ok, title} = Player.play_track(payload)
+
+    {:noreply, assign(socket, :track_started_playing, title)}
   end
 end
