f012490f171a252be3909fb2834732a4be591028
David Oliver <david@doliver.co.uk>
2024-02-06 17:59:36 +0100

    Progress templates and start CSS

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,6 +15,8 @@
 //     import "some-package"
 //
 
+import "../app.css"
+
 // Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
 import "phoenix_html"
 // Establish Phoenix Socket and LiveView configuration.
@@ -38,4 +40,3 @@ liveSocket.connect()
 // >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
 // >> liveSocket.disableLatencySim()
 window.liveSocket = liveSocket
-
