2a61e064ffe21aa87fd754af83a5d7327ce44b65
David Oliver <david@doliver.co.uk>
2024-02-22 15:27:03 +0100

    Reinstate alias and delegations to LiveCue
    I missed this when migrating to a fresh installation of Phoenix 1.7.

--- lib/live_cue.ex
+++ lib/live_cue.ex
@@ -6,4 +6,10 @@ defmodule LiveCue do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+
+  alias LiveCue.{Collection}
+
+  defdelegate(process_collection(), [to: Collection])
+  defdelegate(parse_collection_files(), [to: Collection])
+  defdelegate(store_collection_data(), [to: Collection])
 end
